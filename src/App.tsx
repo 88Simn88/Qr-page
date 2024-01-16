c30a339d7a398482a6eba8b274bfc82751b200df
88simn88 <simnsanchez@yahoo.com.ar>
2024-01-15 23:14:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='page-container'>
+      <div className='general-container'>
+        <div className='image-container'>
+          <img className='img-qr' src="./img/image-qr-code.png" alt="" />
+        </div>
+        <div className='text-container'>
+          <p className='text-1'>
+            Improve your front-end 
+            
+            skills by building projects
+          </p>
+          <p className='text-2'>
+            Scan the Qr code to visit Frontend Mentor and take your coding skills to 
+            the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
